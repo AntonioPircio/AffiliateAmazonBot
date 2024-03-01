675963dc840da042d557dc021947bf2b6f6f0b81
AntonioPircio <antoniopircio@gmail.com>
2024-03-01 20:45:30 +0100

    Added and updated variables

--- constant.py
+++ constant.py
@@ -1,8 +1,10 @@
 baseURL = 'https://www.amazon.it/'
-URLaffiliateelement = '/?tag='
-URLfinalelement = '&psc=1'
+URLAffiliateElement = '/?tag='
+URLfinalElement = '&psc=1'
 telegramToken = '6273547056:AAHb_StXIsyakpC023tE2Zf1CWVmBL-flTs'
-key = b'uk-6u7TnNOJ5-O2GV05q5nI90_-vpA0xdtz5A3yzGd0='
+key = ""
+systemOne = "Linux"
+days = 2
 channel_id = ''
 affiliateCode = ''
 channelLink = ''
